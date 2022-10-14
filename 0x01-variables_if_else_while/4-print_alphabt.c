b745438afbcaed3bb5e57e9528e43d5eed2e2b09
ridwanyah24 <ridwangames24@gmail.com>
2022-10-14 15:48:58 +0000

    new

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int lowerNotAllChar;
 
-	for (lowerNotAllChar = 0x61; lowerNotAllChar < 0x7B; lowerNotAllChar++)
+	while (lowerNotAllChar != 0x65 || lowerNotAllChar != 0x71)
 	{
-		while (lowerNotAllChar != 0x65 || lowerNotAllChar != 0x71)
+		for (lowerNotAllChar = 0x61; lowerNotAllChar < 0x7B; lowerNotAllChar++)
 		{
 			putchar(lowerNotAllChar);
 		}
