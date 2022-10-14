d51deae0aaed97363832db9e3798bafaffc0eac0
ridwanyah24 <ridwangames24@gmail.com>
2022-10-14 14:28:03 +0000

    new

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,20 +9,20 @@
 int main(void)
 {
 	int n;
-	int modulo;
+	int mo;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	modulo = n % 10;
-	if (modulo == 0)
+	mo = n % 10;
+	if (mo == 0)
 	{
-		printf("Last digit of %d is %d", n, modulo + " and is 0");
-	} else if (modulo > 5)
+		printf("Last digit of %d is %d", n, mo + " and is 0");
+	} else if (mo > 5)
 	{
-		printf("Last digit of %d is %d", n, modulo + "and is greater than 5");
-	} else if (modulo < 6 && != 0)
+		printf("Last digit of %d is %d", n, mo + "and is greater than 5");
+	} else if (mo < 6 && mo != 0)
 	{
-		printf("Last digit of %d is %d", n, modulo + "and is less than 6 and not 0");
+		printf("Last digit of %d is %d", n, mo + "and is less than 6 and not 0");
 	}
 	return (0);
 }
