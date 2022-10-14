3e00c1316e5a38c9c72d041b5c9909b9ada67dbb
ridwanyah24 <ridwangames24@gmail.com>
2022-10-14 17:56:38 +0000

    new

---
+++ 0x01-variables_if_else_while/7-print_telbahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - code runs here
+  *
+  *Return: code returns 0
+  */
+int main(void)
+{
+	char small = 'z';
+
+	for (small = 'z'; small >= 'a'; sma--)
+	{
+		putchar(small);
+	}
+	putchar(0x0A);
+
+	return (0);
+}
