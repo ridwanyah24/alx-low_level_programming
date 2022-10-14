c547faa2104eb5b02f1be7a071ed7829b88d985b
ridwanyah24 <ridwangames24@gmail.com>
2022-10-14 22:04:43 +0000

    new

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - code runs here
+  *Return: Function returns 0
+  */
+int main(void)
+{
+	int i, j;
+	int k, l;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			putchar(' ');
+			for (k = '0'; k <= '9'; k++)
+			{
+				for (l = '0'; l <= '9'; l++)
+				{
+					if (i != k && j != l)
+					{
+						putchar(k);
+						putchar(l);
+					}
+					if (i == '9' && j == '8' && k == '9' && 'l' == '9')
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
