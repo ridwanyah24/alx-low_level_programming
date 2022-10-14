e325e5b4c1112704f3674c3254804b5725aad492
ridwanyah24 <ridwangames24@gmail.com>
2022-10-14 17:39:01 +0000

    new

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - code runs here
+  *
+  *Return: function returns a zero
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return (0);
+}
