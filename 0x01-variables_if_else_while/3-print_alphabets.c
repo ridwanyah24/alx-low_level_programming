97a93434f3bd373bdf9d2ad13fddb568adeea12c
ridwanyah24 <ridwangames24@gmail.com>
2022-10-14 15:15:06 +0000

    new

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main - code runs here
+  *
+  *Returns: Code returns 0
+  */
+int main (void)
+{
+	int upperChar;
+	int lowerChar;
+
+	for (lowerChar = 0x61; upperChar < 0x7B; lowerChar++)
+	{
+		putchar(lowerChar);
+	}
+	for (upperChar = 0x41; upperChar < 0x5B; upperChar++)
+	{
+		putchar(upperChar);
+	}
+	putchar(0x0A);
+	
+	return (0);
+}
