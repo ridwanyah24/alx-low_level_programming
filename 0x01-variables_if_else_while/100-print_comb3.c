faae74d7dd810227169c823312767a658a87dc01
ridwanyah24 <ridwangames24@gmail.com>
2022-10-14 20:43:53 +0000

    new

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,11 +17,11 @@ int main(void)
 			{
 				putchar(i);
 				putchar(j);
-				if (i != '8' && j != '9')
-				{
-					putchar(',');
-					putchar(' ');
-				}
+			}
+			if (i != 8 && j != 9)
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
