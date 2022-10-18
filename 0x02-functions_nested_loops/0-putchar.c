6a10ed664544aec1dabff59d31ecf9c206acc6d8
ridwanyah24 <ridwangames24@gmail.com>
2022-10-18 09:55:42 +0000

    new

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - The code runs here
+  *Return: the code returns 0
+  */
+int main(void)
+{
+	char arr[] = {'_','p','u','c','h','a','r'};
+	
+	for (int i = 0; i <=6; i++)
+	{
+		putchar(arr[i]);
+	}
+
+	return (0);
+}
