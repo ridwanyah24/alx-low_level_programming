12b587b9898ee8e94c7f22b477acb2fa38730017
ridwanyah24 <ridwangames24@gmail.com>
2022-10-18 21:14:42 +0000

    new

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+/**
+  * print_times_table - print time table for n nums
+  *@n: the number of tables to be printed
+  *
+  *Return: Code returns void
+  */
+void print_times_table(int n)
+{
+	int begin;
+	int stopper;
+	int muller;
+	for (begin = 0; begin <= n; begin++)
+	{
+		for (stopper = 1; stopper <= n; stopper++)
+		{
+			muller = begin * stopper;
+			printf ("%d, ", muller);
+		}
+	}
+}
