d3c3e8a6975e00f83465a83e1cfa30dc0bfc3259
ridwanyah24 <ridwangames24@gmail.com>
2022-10-18 12:22:48 +0000

    new

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "main.h"
+/**
+  * print_alphabet_x10 - function prints lowercase alphabets 10 times
+  *
+  *Return : function returns void
+  */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (1 < 10)
+	{
+		int j;
+
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
