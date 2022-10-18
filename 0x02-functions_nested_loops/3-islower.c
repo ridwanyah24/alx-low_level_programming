6bdb978e2a1cf8a5420c1eff63fca64d9b0a3cd2
ridwanyah24 <ridwangames24@gmail.com>
2022-10-18 12:37:30 +0000

    new

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <ctype.h>
+#include "main.h"
+/**
+  * _islower - checks if a letter is lowercase
+  *@parameter: integer
+  *
+  *Return : retuns 1 if letter is lower else returns 0
+  */
+int _islower(int)
+{
+	if (islower(int))
+	{
+		return (1);
+	} else 
+	{
+		return (0);
+	}
+}
