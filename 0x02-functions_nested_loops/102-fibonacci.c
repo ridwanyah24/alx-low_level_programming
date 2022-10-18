7108040d1ea941e29d7c9fdf5436d6acc7ffe860
ridwanyah24 <ridwangames24@gmail.com>
2022-10-18 22:23:12 +0000

    new

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,7 +14,7 @@ int main(void)
 	n2 = 2;
 	boolean =  boolean2 = 1;
 	printf("%ld, %ld", n1, n2);
-	for (c = 0; c < 96; c++)
+	for (c = 0; c < 51; c++)
 	{
 		if (boolean)
 		{
@@ -27,20 +27,20 @@ int main(void)
 		{
 			if (boolean2)
 			{
-				n11 = n1 % 500000000;
-				n22 = n2 % 500000000;
-				n1 = n1 / 500000000;
-				n2 = n2 / 500000000;
+				n11 = n1 % 1000000000;
+				n22 = n2 % 1000000000;
+				n1 = n1 / 1000000000;
+				n2 = n2 / 1000000000;
 				boolean2 = 0;
 			}
 			fn2 = (n11 + n22);
-			fn = n1 + n2 + (fn2 / 500000000);
+			fn = n1 + n2 + (fn2 / 1000000000);
 			printf(", %ld", fn);
-			printf("%ld", fn2 % 500000000);
+			printf("%ld", fn2 % 1000000000);
 			n1 = n2;
 			n11 = n22;
 			n2 = fn;
-			n22 = (fn2 % 500000000);
+			n22 = (fn2 % 1000000000);
 		}
 		if (((n1 + n2) < 0) && boolean == 1)
 			boolean = 0;
