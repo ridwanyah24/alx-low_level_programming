f68c746ee6471c2c7bf573b23ef5125d342ad78a
ridwanyah24 <ridwangames24@gmail.com>
2022-10-18 20:43:08 +0000

    new

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,7 +7,7 @@
   */
 void print_to_98(int n)
 {
-	if (n > 98)
+	if (n >= 98)
 	{
 		for (n = n; n >= 98; n++)
 		{
@@ -23,7 +23,7 @@ void print_to_98(int n)
 				_putchar(' ');
 			}
 		}
-	} else if (n < 98)
+	} else if (n <= 98)
 	{
 		for (n = n; n <= 98; n++)
 		{
