021117a554d21dc4c252faf9fcc4a0c48879b864
ridwanyah24 <ridwangames24@gmail.com>
2022-10-18 16:18:33 +0000

    new

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,10 +2,13 @@
 #include "main.h"
 /**
   * print_last_digit - prints the last digit of a number
-  *@c: the number to get its last digit
+  *@l: the number to get its last digit
   *Return: returns the last digit of the number
   */
 int print_last_digit(int l)
 {
-	return (l % 10);
+	int j;
+
+	j = l % 10;
+	return (j);
 }
