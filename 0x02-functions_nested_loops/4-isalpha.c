0d9ec1c6e51d76bb486a71e8978e6f991333032b
ridwanyah24 <ridwangames24@gmail.com>
2022-10-18 14:21:31 +0000

    new

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+/**
+  * _isalpha - check if letters are in alphabetical order
+  *@c : integer
+  *Return: function returns 1 if c is a letter, lowercase or uppercase
+  */
+int _isalpha(int c)
+{
+	if (isalpah(c))
+	{
+		return (1);
+	}
+	return (0);
+}
