d3f6a293d6ba3af30ab66e343ab282556c700a6b
ridwanyah24 <ridwangames24@gmail.com>
2022-10-19 12:52:36 +0000

    new

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+/**
+  * main - code runs here
+  *
+  *Return: always 0
+  */
+int main(void)
+{
+	positive_or_negative(0);
+	return (0);
+}
