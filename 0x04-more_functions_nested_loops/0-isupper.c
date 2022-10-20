f0d982702de9b1e599ac37a211f8a2addc0f494c
ridwanyah24 <ridwangames24@gmail.com>
2022-10-20 12:37:54 +0000

    new

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+  * _isupper - print uppercase character
+  *@c: character to be printed
+  *
+  *Return:0 if lowercase 1 if uppercase
+  */
+int _isupper(int c)
+{
+	if (ispper(c))
+	{
+		return (1);
+	}
+
+	return (0);
+}
