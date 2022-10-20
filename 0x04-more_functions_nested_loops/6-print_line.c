298bc671cd69185ad48a9d3350173ccc89005e24
ridwanyah24 <ridwangames24@gmail.com>
2022-10-20 16:47:06 +0000

    new

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * print_line - print a straight line
+  *@n: number of times the underscore should print
+  *
+  *Return: returns void
+  */
+void print_line(int n)
+{
+	int count = 0;
+
+	while (count < n)
+	{
+		_putchar('_');
+		count++;
+	}
+}
