98c3c12f6a9945c0240687f27d2d2418ca6e143a
ridwanyah24 <ridwangames24@gmail.com>
2022-10-20 13:08:01 +0000

    new

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+/**
+  * _isdigit - check if a number is from 0-9
+  *@c: input param
+  *
+  *Return: 1 if digit 0 otherwise
+  */
+int _isdigit(int c)
+{
+	if(c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+
+	return (0);
+}
