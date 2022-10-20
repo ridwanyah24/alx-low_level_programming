efb6c6125421044ef8da16ccd14aa73818db6244
ridwanyah24 <ridwangames24@gmail.com>
2022-10-20 20:42:23 +0000

    new

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,42 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-/**
-  * main - code runs here
-  * Return: always 0
-  */
-void fizz_buzz(void);
 
+/**
+ * main - prints Buzz each numbers of 3 and 5.
+ * Return: Always 0.
+ */
 int main(void)
 {
-	fizz_buzz();
-	return (0);
-}
-/**
-  * fizz_buzz - print numbers from 1 -100 except multiple of 3
-  * and 5 and replacing them with
-  * fizz for 3 and buzz for 5 and fizzbuzz for multiple of both
-  * Return: returns void
-  */
-void fizz_buzz(void)
-{
-	int i;
+	int n;
 
-	for (i = 1; i < 101; i++)
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n <= 100; n++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
+		if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf("Fizz ");
-		} else if ((i % 5 == 0) && i % 3 != 0)
+			printf(" FizzBuzz");
+		}
+		else if (n % 3 == 0)
 		{
-			printf("Buzz ");
-		} else if (i % 3 == 0 && i % 5 == 0)
+			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
 		{
-			printf("FizzBuzz ");
-		} else
+			printf(" Buzz");
+		}
+		else
 		{
-			printf("%d ", i);
+			printf(" %d", n);
 		}
 	}
 	printf("\n");
+	return (0);
 }
