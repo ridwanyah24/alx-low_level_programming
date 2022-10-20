e3c42d067d395dbd4b6868313e83b7b64077449b
ridwanyah24 <ridwangames24@gmail.com>
2022-10-20 16:31:27 +0000

    new

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,15 @@
   */
 void more_numbers(void)
 {
-	int i;
-	int j;
+	int i, ch;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (ch = 0; ch < 15; ch++)
 		{
-			if (j > 9)
-			{
-				_putchar((j / 10) + 48);
-			}
-			_putchar((j % 10) + 48)
+			if (ch >= 10)
+				_putchar((ch / 10) + 48);
+			_putchar((ch % 10) + 48);
 		}
 		_putchar('\n');
 	}
