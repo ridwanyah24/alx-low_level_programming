a75cbe0d3d281bb5d2f1c3d3dba3d73f11c30d5c
ridwanyah24 <ridwangames24@gmail.com>
2022-10-20 15:20:13 +0000

    new

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+/**
+  * print_numbers - prints numbers from 0-9
+  *
+  *Return: always zero
+  */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
