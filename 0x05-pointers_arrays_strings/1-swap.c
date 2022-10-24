90839fac9fcfbe5e59798af6c051dd7ddb0eaf9c
ridwanyah24 <ridwangames24@gmail.com>
2022-10-24 15:26:12 +0000

    new

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+/**
+  * swap_int - swaps the value of int a for the value of int b
+  *@a: pointer to int a
+  *@b: pointer to int b
+  *Return: void
+  */
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
