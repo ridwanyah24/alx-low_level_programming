6fe23bc50f9ada31f29f8b75b6c449df0de59359
ridwanyah24 <ridwangames24@gmail.com>
2022-10-24 18:13:44 +0000

    new

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,11 +10,11 @@
 void rev_string(char *s)
 {
 	int i;
-	int j;
+	long int j;
 
 	for (i = strlen(s) - 1; i >= 0; i--)
 	{
-		for (j = 0; j < strlen(s) - 1; j++)
+		for (j = 0; j < (strlen(s) - 1); j++)
 		{
 			*(s + j) = s[i];
 		}
