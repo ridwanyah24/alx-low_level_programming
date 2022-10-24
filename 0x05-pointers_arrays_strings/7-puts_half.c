9bad0fef9e04cb423e5c5e937b6e7e5fc65d05ac
ridwanyah24 <ridwangames24@gmail.com>
2022-10-24 20:41:42 +0000

    new

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * puts_half - prints half the chars in a string
+  *@str: the string
+  *Return: none
+  */
+void puts_half(char *str)
+{
+	int count = 0;
+	int i;
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+			break;
+		count++;
+	}
+	count /= 2;
+	for (i = 0; i <= count; i++)
+	{
+		_putchar(str[count]);
+	}
+}
