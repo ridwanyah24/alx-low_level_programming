8fbff4f8048bd8d88285ba59a6add556afbdd149
ridwanyah24 <ridwangames24@gmail.com>
2022-10-24 16:09:58 +0000

    n

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+  * _puts - prints the string to standard output
+  *@str - the string to be printed
+  *Return: void
+  */
+void _puts(char *str)
+{
+	write(1, &str, strlen(str));
+}
