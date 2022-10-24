1c6c66e9a4974001bec529dd3520f26d890ace09
ridwanyah24 <ridwangames24@gmail.com>
2022-10-24 21:40:25 +0000

    ne

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,9 +14,9 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", *(a + i));
-		if (i == n - 1)
-			break;
+		printf("%d", *(a + i));
+		if (i != n - 1)
+			printf(", ");
 	}
 	printf("\n");
 }
