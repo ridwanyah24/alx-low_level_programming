66436cf49cfa53ad2e380a33be8486fc5340886b
ridwanyah24 <ridwangames24@gmail.com>
2022-10-24 22:14:11 +0000

    new

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+  * _atoi - converts a string into an integer not explicit
+  *@s: the string to be converted
+  *Return: always 0;
+  */
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
