9830f1b79600bd532837690cf15cbf4869c05c14
ridwanyah24 <ridwangames24@gmail.com>
2022-10-24 19:48:43 +0000

    new

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * puts2 - prints every other character in a string
+  *@n: the string
+  *Return: void
+  */
+void puts2(char *str)
+{
+	unsigned int i;
+
+	for (i = 0; i <= strlen(str) - 1; i += 2)
+	{
+		_putchar(str[i]);
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ void _puts(char *str);
 void print_rev(char *s);
 int _putchar(char);
 void rev_string(char *s);
+void puts2(char *str);
