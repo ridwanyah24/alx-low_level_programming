9e5931d355f02674b5866c74f9a7715c5686d635
ridwanyah24 <ridwangames24@gmail.com>
2022-10-24 22:53:33 +0000

    n

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,81 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <math.h>
+
 /**
- * randomPasswordGeneration - generates keygen.
- * Return: void Always.
+ * main generates a keygen
+ * Return: 0
  */
-void randomPasswordGeneration(int N)
+int main(void)
 {
-	 // Initialize counter
-    int i = 0;
+	int a, b;
 
-    int randomizer = 0;
+	a = 0;
 
-    // Seed the random-number generator
-    // with current time so that the
-    // numbers will be different every time
-    srand((unsigned int)(time(NULL)));
+	srand((unsigned int)time(NULL));
 
-    // Array of numbers
-    char numbers[] = "0123456789";
+	for (b = 0; b < 2772; b = b + a)
+	{
+		a = rand() % 128;
 
-    // Array of small alphabets
-    char letter[] = "abcdefghijklmnoqprstuvwyzx";
+		if (a + b > 2772)
+			break;
 
-    // Array of capital alphabets
-    char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+		printf("%c", a);
+	}
+	printf("%c\n", 2772 - b);
 
-    // Array of all the special symbols
-    char symbols[] = "!@#$^&*?";
-
-    // Stores the random password
-    char password[N];
-
-    // To select the randomizer
-    // inside the loop
-    randomizer = rand() % 4;
-
-    // Iterate over the range [0, N]
-    for (i = 0; i < N; i++) {
-
-        if (randomizer == 1) {
-            password[i] = numbers[rand() % 10];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else if (randomizer == 2) {
-            password[i] = symbols[rand() % 8];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else if (randomizer == 3) {
-            password[i] = LETTER[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else {
-            password[i] = letter[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-    }
-}
-
-/**
-  * main - code runs here
-  *Return: 0
-  */
-int main()
-{
-    // Length of the password to
-    // be generated
-    int N = 10;
-
-    // Function Call
-    randomPasswordGeneration(N);
-
-    return 0;
-}
+	return(0);
 }
