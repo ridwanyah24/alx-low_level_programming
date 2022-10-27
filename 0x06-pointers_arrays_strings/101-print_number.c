b4f54064a3062d2b80234e9325b564e29a59bd08
ridwanyah24 <ridwangames24@gmail.com>
2022-10-27 16:54:12 +0000

    n

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * print_number - prints an integer
+  *@n: the integer to be printed;
+  *Return: void
+  */
+void print_number(int n)
+{
+	int count = 1;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	d = n;
+	while (d < 10)
+	{
+		_putchar(d + 48);
+		break;
+	}
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+	d = n;
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
+}
