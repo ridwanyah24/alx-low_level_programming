5529762d154d7d31f7109f4cc464523ac936a659
ridwanyah24 <ridwangames24@gmail.com>
2022-10-31 16:32:38 +0000

    new

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+  * _strchr - locates a character in a string
+  *@s: the string
+  *@c: the character
+  *Return: the pointer to the string
+  */
+char *_strchr(char *s, char c)
+{
+	strchr(s, c);
+	return (s);
+}
