e5889ba65243e3c83580320741ce48b1a55db038
ridwanyah24 <ridwangames24@gmail.com>
2022-10-31 14:59:14 +0000

    n

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 /**
-  * _memset - fills the first n bytes of the memory areay pointed to 
+  * _memset - fills the first n bytes of the memory areay pointed to
   *by s with the constant byte b
   *@s: the string s pointer
   *@b: the constant byte b
@@ -11,7 +11,7 @@
   */
 char *_memset(char *s, char b, unsigned int n)
 {
-	memset(s,b,n);
+	memset(s, b, n);
 	return (s);
 }
 
