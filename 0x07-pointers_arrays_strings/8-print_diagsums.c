d3e77b98f753aa9b0ad3ce22a12e91a5c0fc6a6d
ridwanyah24 <ridwangames24@gmail.com>
2022-10-31 19:50:00 +0000

    n

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,17 +9,14 @@
   */
 void print_diagsums(int *a, int size)
 {
-	int i;
-	int sum = 0;
-	int sum1 = 0;
+	int i, sum1 = 0, sum2 = 0;
 
-
-	for (i = 0; i < size * size; i++)
+	for (i = 0; i < (size * size); i++)
 	{
-		if (i % (sum + 1) == 0)
-			sum = sum + *(a + i);
+		if (i % (size + 1) == 0)
+			sum1 += *(a + i);
 		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
-			sum1 = sum1 + *(a + i);
+			sum2 += *(a + i);
 	}
-	printf("%d, %d\n", sum, sum1);
+	printf("%d, %d\n", sum1, sum2);
 }
