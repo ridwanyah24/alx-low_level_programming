d395c8923354041520d0bdd8754f599b754d0b64
ridwanyah24 <ridwangames24@gmail.com>
2022-11-02 11:04:43 +0000

    n

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+  * _print_rev_recursion - prints a string in reverse
+  *@s: the string
+  *Return: void
+  */
+void _print_rev_recursion(char *s)
+{
+	unsigned int i = strlen(s) - 1;
+
+	if (i >= 0)
+	{
+		_putchar((*s + i));
+		_put_rev_recursion(s - 1);
+	}
+	_putchar('\n');
+}
