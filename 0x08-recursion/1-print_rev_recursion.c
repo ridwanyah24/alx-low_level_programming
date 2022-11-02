107a1652eabfed36181c1595810b1ab95a4d0830
ridwanyah24 <ridwangames24@gmail.com>
2022-11-02 11:31:26 +0000

    n

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,11 +9,12 @@
 void _print_rev_recursion(char *s)
 {
 	unsigned int i = strlen(s) - 1;
+
 	if (*s != '\0')
 	{
 		_putchar(*(s + i));
 		i--;
-		_print_rev_recursion(s - 1);
+		_print_rev_recursion(s + 1);
 	}
 	_putchar('\n');
 }
