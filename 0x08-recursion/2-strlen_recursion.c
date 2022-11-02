098865724249d17fc9c01235e75dfccd2f88b4f8
ridwanyah24 <ridwangames24@gmail.com>
2022-11-02 12:02:45 +0000

    n

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,12 +7,12 @@
   */
 int _strlen_recursion(char *s)
 {
-	int  i = 1;
-
+	int len = 0;
 	if (*s != '\0')
 	{
-		_strlen_recursion(s + 1);
-		i++;
-	}
-	return (i);
+		len++;
+		_strlen_recursion(s + len);
+	} else
+		len = 0;
+	return (len);
 }
