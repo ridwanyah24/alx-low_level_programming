aef6a08957f6f6c9c2c47299e1b9da079090809c
ridwanyah24 <ridwangames24@gmail.com>
2022-11-04 14:27:03 +0000

    N

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,16 +7,16 @@
   *@argv: argument value
   *Return: return 0
   */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int result = 0;
 	int i, j;
 
-	for(i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if(!isdigit(argv[i][j]))
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
