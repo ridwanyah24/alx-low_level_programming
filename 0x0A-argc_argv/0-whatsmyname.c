7cd8ff9996f9e64cf3dff5575cfb96328c398278
ridwanyah24 <ridwangames24@gmail.com>
2022-11-04 10:21:47 +0000

    N:

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - code runs here
+  *@argc: argument count
+  *@argv: argument value
+  *Return: always 0
+  */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[argc -1]);
+	return (0);
+}
