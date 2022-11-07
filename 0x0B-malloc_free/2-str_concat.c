863fab94032076c6514fcf6a03c868c8b7428931
ridwanyah24 <ridwangames24@gmail.com>
2022-11-07 19:23:51 +0000

    n

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+  * str_concat - concatenates two strings
+  *@s1: string 1
+  *@s2: string 2
+  *Return: pointer to s1;
+  */
+char *str_concat(char *s1, char *s2)
+{
+	char *str;
+	unsigned int i, j, k = 0;
+
+	i = strlen(s1) + strlen(s2);
+	if (s1 == NULL)
+		s1 = "";
+	if(s2 == NULL)
+		s2 = "";
+	str = malloc((sizeof(char) * i) + 1);
+	if (str == NULL)
+		return (NULL);
+	for (j = 0; j < strlen(s1); j++)
+	{
+		str[j] = s1[j];
+	}
+	for (; j < i; j++)
+	{
+		str[j] = s2[k];
+		k++;
+		if (k >= strlen(s2))
+			break;
+	}
+	return (str);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,5 +6,5 @@
 
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
-
+char *str_concat(char *s1, char *s2);
 #endif
