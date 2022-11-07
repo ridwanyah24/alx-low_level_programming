cfc906e13d42e7fc0be06dd1bfdff4e8e6347e07
ridwanyah24 <ridwangames24@gmail.com>
2022-11-07 20:38:46 +0000

    n

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,7 +12,7 @@ int **alloc_grid(int width, int height)
 
 	if(width <= 0 || height <= 0)
 		return (NULL);
-	array = malloc(sizeof(*array) * (width * height));
+	array = malloc(sizeof(int) * (width * height));
 	if (array == NULL)
 	{
 		free(array);
