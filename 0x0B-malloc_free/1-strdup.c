672ef5f54fc1c66db892d6ddbacc5ef83566b7aa
ridwanyah24 <ridwangames24@gmail.com>
2022-11-07 18:34:22 +0000

    n

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  *_strdup - returns a pointer to a space in memory contaiing str
+  *@str: the string the space contains
+  *Return: pointer to char
+  */
+char _strdup(char *str)
+{
+	char *str1;
+	unsigned int i;
+	
+	i = strlen(str);
+	
+	str1 = malloc(sizeof(char) * i);
+	if (malloc(sizeof(char) * i) == NULL)
+		return (NULL);
+	for (j = 0; j < i; j++)
+	{
+		str1[j] = str[j];
+	}
+	return (str1);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
 
 #endif
