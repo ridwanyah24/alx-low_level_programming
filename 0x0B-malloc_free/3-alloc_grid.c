75009d548784dfce40425fb3670892550abfc606
ridwanyah24 <ridwangames24@gmail.com>
2022-11-07 20:48:36 +0000

    n

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,9 +18,9 @@ int **alloc_grid(int width, int height)
 		free(array);
 		return (NULL);
 	}
-	for (i = 0; i < (width * height); i++)
+	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < (width * height); j++)
+		for (j = 0; j < width; j++)
 		{
 			array[i][j] = 0;
 		}
