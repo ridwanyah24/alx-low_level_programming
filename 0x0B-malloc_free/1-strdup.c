a46150f51bb289788f6a9b6c89a980c7111da1f2
ridwanyah24 <ridwangames24@gmail.com>
2022-11-07 18:55:52 +0000

    n

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,13 +9,11 @@ char *_strdup(char *str)
 	char *str1;
 	unsigned int i, j;
 
+	if (str == NULL)
+		return (NULL);
 	i = strlen(str);
 	str1 = malloc((sizeof(*str) * i) + 1);
 	if (str1 == NULL)
-	{
-		return (NULL);
-	}
-	if (str == NULL)
 		return (NULL);
 	for (j = 0; j < i; j++)
 	{
