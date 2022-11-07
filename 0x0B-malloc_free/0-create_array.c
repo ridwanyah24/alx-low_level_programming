840e5d58ef0dde88bafa6c5e92a4ca37ac551dc3
ridwanyah24 <ridwangames24@gmail.com>
2022-11-07 16:47:10 +0000

    n

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,6 +10,10 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int i;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	s = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 	{
