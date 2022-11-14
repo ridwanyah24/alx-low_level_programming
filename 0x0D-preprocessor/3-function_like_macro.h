62443b60e5fcd67aa89d7dde4f81a5711fad4a12
ridwanyah24 <ridwangames24@gmail.com>
2022-11-14 13:41:01 +0000

    n

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,13 @@
-#ifndef FUNC_H
-#define FUNC_H
-#include <stdlib.h>
-#define ABS(x) (abs(x))
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/*
+ * File: 3-function_like_macro.h
+ * Auth: Yahya Ridwan
+ * Desc: Header file that defines a function-like macro ABS(x)
+ *       that computes the absolute value of a number x.
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
 #endif
