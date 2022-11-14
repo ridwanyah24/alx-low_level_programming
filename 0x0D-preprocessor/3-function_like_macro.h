d8399da77b36521f89af38f2958ca918a2b4ac54
ridwanyah24 <ridwangames24@gmail.com>
2022-11-14 13:23:58 +0000

    n

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,5 +2,10 @@
 #define FUNC_H
 #include <stdlib.h>
 #include <stdio.h>
-#define ABS(X) (abs((X)))
+#define ABS(X) abs(X)
+/**
+  * absolute - returns absolute value
+  *@x: the number
+  *Return: x
+  */
 #endif
