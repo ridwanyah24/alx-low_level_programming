9435d9d18dc791325906aeadb79b503d057c840c
ridwanyah24 <ridwangames24@gmail.com>
2022-11-14 20:41:31 +0000

    n

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,20 +1,50 @@
 #include "dog.h"
+
 /**
-  *new_dog - creates a new dog
-  *@name: name
-  *@age: age
-  *@owner: owner
-  *Return: pointer to struct dog
-  */
+ * new_dog - creates a new dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: the new dog greated or null if failed
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p;
-	
-	p = malloc(sizeof(dog_t));
-	if (p == NULL)
+	int name_len, owner_len, i;
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+
+	if (dog == NULL || !name || !owner)
+	{
+		free(dog);
 		return (NULL);
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
-	return (p);
+	}
+
+	name_len = strlen(name);
+	owner_len = strlen(owner);
+
+	dog->name = malloc(name_len + 1);
+	dog->owner = malloc(owner_len + 1);
+
+	if (!(dog->name) || !(dog->owner))
+	{
+		free(dog->name);
+		free(dog->owner);
+		free(dog);
+
+		return (NULL);
+	}
+
+	for (i = 0; i < name_len; i++)
+		dog->name[i] = name[i];
+	dog->name[i] = '\0';
+
+	dog->age = age;
+
+	for (i = 0; i < owner_len; i++)
+		dog->owner[i] = owner[i];
+	dog->owner[i] = '\0';
+
+	return (dog);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 /**
