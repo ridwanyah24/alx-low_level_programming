27d3e5d315b681e5024f36d07c0308d65b2918f5
ridwanyah24 <ridwangames24@gmail.com>
2022-11-14 13:32:25 +0000

    n

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
 #ifndef FUNC_H
 #define FUNC_H
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define ABS(x) abs((x)) //((x) < 0 ? (-x) : (x))
 #endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,3 @@
+#ifndef FUNC_H
+#define FUNC_H
+#define SUM(x, y)
