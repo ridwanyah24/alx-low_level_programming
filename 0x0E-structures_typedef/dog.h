83c7ab3457274a9c6441bf46a9a7f102486ff0ef
ridwanyah24 <ridwangames24@gmail.com>
2022-11-14 19:28:43 +0000

    n

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
 /**
   *struct dog - data structure for a dog
   *@name: name of dog
@@ -13,4 +15,5 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
