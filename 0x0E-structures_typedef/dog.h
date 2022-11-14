a41df0c8fbd8e6784114c68f82ba2b0ec9071a7d
ridwanyah24 <ridwangames24@gmail.com>
2022-11-14 18:32:25 +0000

    n

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  *struct dog - data structure for a dog
+  *@name: name of dog
+  *@age: its age
+  *@owner: its owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
