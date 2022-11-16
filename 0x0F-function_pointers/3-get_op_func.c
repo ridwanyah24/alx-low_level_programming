02b7dd562f8a2fa58b3c56f140f4ed5e6bcb147e
ridwanyah24 <ridwangames24@gmail.com>
2022-11-16 19:37:58 +0000

    n

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -6,8 +6,7 @@
   */
 int (*get_op_func(char *s))(int a, int b)
 {
-	op_t ops[] = 
-	{
+	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
@@ -16,7 +15,7 @@ int (*get_op_func(char *s))(int a, int b)
 		{NULL, NULL}
 	};
 	int i = 0;
-	
+
 	while (ops[i].op != NULL && *(ops[i].op) != *s)
 	{
 		i++;
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -4,7 +4,7 @@
   *@b: int b
   *Return: a + b
   */
-int op_add (int a, int b)
+int op_add(int a, int b)
 {
 	return (a + b);
 }
