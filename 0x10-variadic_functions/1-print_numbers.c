fda1806013787d0a61d2ecbe92513d8fbc5a0ed2
ridwanyah24 <ridwangames24@gmail.com>
2022-11-17 12:06:36 +0000

    n

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+/**
+  *print_numbers - prints numbers
+  *@seperator: the string to be printed between the numbers
+  *@n: the number of numbers to be printed
+  *
+  */
+void print_numbers(const char *seperator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, j;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (i == n - 1)
+			continue;
+		printf("%s", seperator);
+	}
+	printf("\n");
+	va_end(ap);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,5 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_numbers(const char *seperator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 #endif
