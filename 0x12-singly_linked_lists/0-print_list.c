ac02fb16402198f541e81deb11c06fe08b913c0a
ridwanyah24 <ridwangames24@gmail.com>
2022-11-24 12:09:51 +0000

    n

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,19 +8,21 @@
   */
 size_t print_list(const list_t *head)
 {
-	unsigned int count;
+	size_t count = 0;
 	char *strholder;
+	unsigned int num;
 
 	while (head != NULL)
 	{
+		count++;
 		strholder = head->str;
-		count = head->len;
+		num = head->len;
 		if (strholder == NULL)
 		{
-			count = 0;
+			num = 0;
 			strholder = "(nil)";
 		}
-		printf("[%d] %s\n", count, strholder);
+		printf("[%d] %s\n", num, strholder);
 		head = head->next;
 	}
 	return (count);
