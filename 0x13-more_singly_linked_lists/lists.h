775c3925a0e38150a3d8dde5a53bfb3511f0ded5
ridwanyah24 <ridwangames24@gmail.com>
2022-11-30 09:13:42 +0000

    n

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,6 +15,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+size_t free_listint_safe(listint_t **h);
 size_t print_listint_safe(const listint_t *head);
 listint_t *reverse_listint(listint_t **head);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
