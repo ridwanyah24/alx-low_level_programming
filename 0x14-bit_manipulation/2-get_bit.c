475871fbb1cdf2d2179034e236805050d243773e
ridwanyah24 <ridwangames24@gmail.com>
2022-12-01 15:59:53 +0000

    n

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	char bit;
 
-	if (index >= (sizeof(unsigned long int) *8))
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
 	bit = (n >> index) & 1;
