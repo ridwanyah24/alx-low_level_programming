17d98207674a148099295ea138f7b8abc601a66f
ridwanyah24 <ridwangames24@gmail.com>
2022-12-01 15:53:44 +0000

    n

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  *get_bit - gets a bit at the given index
+  *@n: the integer bytes
+  *@index: the index
+  *Return: -1 for failure, and value of bit for success
+  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	char bit;
+
+	bit = (n >> index) & 1;
+
+	return (bit);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,5 +7,8 @@
 #include <math.h>
 #define pow()
 
+int get_bit(unsigned long int n, unsigned int index);
+int _putchar(char c);
+void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 #endif
