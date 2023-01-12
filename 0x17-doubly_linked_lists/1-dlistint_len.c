46b417eb0e4820be4f544cb2f3927d7b193ca1a0
ridwanyah24 <ridwangames24@gmail.com>
2023-01-12 18:19:08 +0000

    n

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+  * dlistint_len - returns the lenght of a doubly linked list
+  *@h: the head node
+  *
+  *Return: the lenght of the doubly linked list
+  */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t node = 0;
+	while (h)
+	{
+		node++;
+		h = h->n;
+	}
+	return (node);
+}
