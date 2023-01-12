b668b23c76576ddba093bd62ebe800c7df006a5a
ridwanyah24 <ridwangames24@gmail.com>
2023-01-12 21:48:19 +0000

    n

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,24 +8,21 @@
   */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node, *temp;
+	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
 	if (*head)
 	{
-		new_node->n = n;
-		new_node->next = *head;
-		new_node->prev = NULL;
-
-		temp = *head;
-		*head = new_node;
-		new_node = temp;
-
-		return (new_node);
+		(*head)->prev = new_node;
 	}
-	return (NULL);
+	*head = new_node;
+
+	return (new_node);
 }
