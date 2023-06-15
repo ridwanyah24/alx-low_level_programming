cfb009d0807f63d0338a615dd8255c8e988842bc
ridwanyah24 <ridwangames24@gmail.com>
2023-06-15 23:08:15 +0000

    n

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,6 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *t = malloc(sizeof(hash_table_t));
 	unsigned long int i;
+
 	t->array = malloc(sizeof(hash_node_t *) * size);
 	t->size = size;
 
